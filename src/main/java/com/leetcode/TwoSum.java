e17e78255d63ab8ed401aa741152c024f6b26746
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-09-03 13:34:12 -0400

    solved leetcode two some in On using hashmap

--- src/main/java/com/leetcode/TwoSum.java
+++ src/main/java/com/leetcode/TwoSum.java
@@ -1,17 +1,22 @@
 package com.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target){
 
-        for(int i = 0; i<nums.length-1; i++){
-           for(int j = i+1; j<nums.length; j++){
-            if(nums[i] + nums[j] == target) {
-                return new int[] {i, j};
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+
+            int compliment = target - nums[i];
+            if(numMap.containsKey(compliment)){
+                return new int[] {numMap.get(compliment), i};
             }
-           }
+
+            numMap.put(nums[i], i);
 
         }
         return null;
